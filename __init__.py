4a7a6b1c3f2a5c7413de5a9ffc7e9c21ad318ed9
paperboyv7 <mitchellmurphy@gmail.com>
2020-05-01 10:42:48 +0000

    misc

--- __init__.py
+++ __init__.py
@@ -8,23 +8,27 @@ class hangboard(MycroftSkill):
     @intent_file_handler('hangboard.intent')
     def handle_hangboard(self, message):
         try:
-            number = int(message.data.get("number"))
-            response = {'number': message.data.get("number")}
-            self.speak_dialog("hangboard_start", data=response)
-            for j in range(1, number+1, +1):
+            hang = int(message.data.get("hang"))
+            response = {'hang': message.data.get("hang")}
+            reps = int(message.data.get("reps"))
+            repsresponse = {'reps': message.data.get("reps")}
+            rest = int(message.data.get("rest"))
+            restresponse = {'rest': message.data.get("rest")}
+            self.speak_dialog("hangboard_restend", data=response)
+            for j in range(1, reps+1, +1):
                 repno = str(j)
                 self.speak_dialog("hangboard_repstart", data={"repno": repno})
 #                self.speak(str(j) + " .")
-                for i in range(number, 0, -1):
-                    self.speak("hang" + " .")
+                for i in range(hang, 0, -1):
+                    self.speak(str(i) + " .")
                     time.sleep(1)
-                if j < number:
-                    self.speak_dialog("hangboard_rest", data=response)
-                    for i in range(number, 0, -1):
+                if j < reps:
+                    self.speak_dialog("hangboard_rest", data=restresponse)
+                    for i in range(rest, 0, -1):
                         time.sleep(1)
                         #self.speak("rest" + " .")
                     self.speak_dialog("hangboard_restend", data=response)
-            self.speak_dialog("hangboard_finish", data=response)
+            self.speak_dialog("hangboard_finish")
             pass
         except:
             self.speak_dialog("hangboard_error")
