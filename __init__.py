4e0d19845c278f81cc9630113e9467a6c8c84c85
paperboyv7 <mitchellmurphy@gmail.com>
2020-05-01 09:31:00 +0000

    add reps

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import time
 
 class hangboard(MycroftSkill):
     def __init__(self):
@@ -11,11 +11,20 @@ class hangboard(MycroftSkill):
             number = int(message.data.get("number"))
             response = {'number': message.data.get("number")}
             self.speak_dialog("hangboard_start", data=response)
-            for i in range(number, 0, -1):
-                self.speak("hang" + " .")
-            for i in range(number, 0, -1):
-                self.speak("rest" + " .")
-            self.speak_dialog("hangboard_stop")
+            for j in range(1, number+1, +1):
+                repno = str(j)
+                self.speak_dialog("hangboard_repstart", data={"repno": repno})
+#                self.speak(str(j) + " .")
+                for i in range(number, 0, -1):
+                    self.speak("hang" + " .")
+                    time.sleep(1)
+                if j < number:
+                    self.speak_dialog("hangboard_rest", data=response)
+                    for i in range(number, 0, -1):
+                        time.sleep(1)
+                        #self.speak("rest" + " .")
+                    self.speak_dialog("hangboard_restend", data=response)
+            self.speak_dialog("hangboard_finish", data=response)
             pass
         except:
             self.speak_dialog("hangboard_error")
